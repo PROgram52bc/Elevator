93ee85e407fb8ae15383a46947b0283ae74dcfd6
David <daviddenghaotian@163.com>
2018-05-29 14:50:34 +0800

    Added message (laowugui) to rawElevatorDemo.cpp

--- RawElevatorDemo.cpp
+++ RawElevatorDemo.cpp
@@ -11,16 +11,21 @@ int main() {
 	elegraphics::clrscr();
 	Elevator my_ele;
 	char signal;
+	elegraphics::secConsoleOut.sendMsg("Welcome to Dimen!");
+	elegraphics::secConsoleIn.focusCursor();
 	while (cin.get(signal)) {
 		switch(signal) {
 			case 'j':
 				my_ele.goDown();
+				elegraphics::secConsoleOut.sendMsg("Laowugui went to dimen.");
 				break;
 			case 'k':
 				my_ele.goUp();
+				elegraphics::secConsoleOut.sendMsg("Laowugui went up.");
 				break;
 			case 'r':
-				my_ele.getOutCustomer();
+				if(my_ele.getOutCustomer())
+					elegraphics::secConsoleOut.sendMsg("Laowugui went away.");
 				break;
 			case '1':
 			case '2':
@@ -31,7 +36,8 @@ int main() {
 			case '7':
 			case '8':
 			case '9':
-				my_ele.getInCustomer(Customer(int(signal)-48));
+				if(my_ele.getInCustomer(Customer(int(signal)-48)))
+					elegraphics::secConsoleOut.sendMsg("Laowugui to floor " + to_string(signal-48) + " added.");
 				break;
 		}
 		elegraphics::secConsoleIn.clrSection();
