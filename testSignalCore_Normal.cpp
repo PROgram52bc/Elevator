a447e1c6fedaeaa2530967f95856aadf90683ddc
David <daviddenghaotian@163.com>
2018-06-07 23:05:09 +0800

    Added an erroneous case to testSignalCore_Normal

--- testSignalCore_Normal.cpp
+++ testSignalCore_Normal.cpp
@@ -6,10 +6,10 @@ int main() {
 	SignalCore_Normal sgn;
 	//sgn.addSignal(2, 4, SignalCore_B::down);
 	//sgn.addSignal(2, 8, SignalCore_B::both);
-	sgn.addSignal(3, 9, SignalCore_B::both);
-	sgn.addSignal(3, 8, SignalCore_B::both);
-	sgn.addSignal(7, 9, SignalCore_B::down);
+	sgn.addSignal(7, 8, SignalCore_B::both);
+	sgn.addSignal(7, 9, SignalCore_B::both);
+	sgn.addSignal(7, 8, SignalCore_B::down);
+	cout << sgn.getListStr() << endl;
 
-	sgn.printList();
 	return 0;
 }
